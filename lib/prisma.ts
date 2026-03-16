efcc2c51eb56c3149587f1f39e1b6a40ec625883
AnhKiet <nguyenanhkietkiet7766@gmail.com>
2026-03-16 17:22:20 +0700

    Final attempt to fix PrismaClient type error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/default';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
